18983bca46296281ffb307d90edf699103b5f400
Mireziz-Memmedov <azzmmmdv@gmail.com>
2025-10-31 17:22:13 +0000

    js

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 $(document).ready(function () {
     $(document).on("keypress", function (e) {
         if (e.which == 13) {
-            let url = `https://apidemo-7jd3lqpmk-mirezizs-projects.vercel.app/api/news`;
+            let url = `https://apidemo-six.vercel.app/api/news`;
             async function mynews() {
                 let responce = await fetch(url);
                 let data = await responce.json();
