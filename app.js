85c867b3b424f7e75bc6ca99f97da233169b95b3
Mireziz-Memmedov <azzmmmdv@gmail.com>
2025-10-31 17:08:34 +0000

    app.js

--- app.js
+++ app.js
@@ -1,13 +1,13 @@
 $(document).ready(function () {
     $(document).on("keypress", function (e) {
         if (e.witch == 13) {
-            let url = `https://apidemo-six.vercel.app/api/news`;
+            let url = `https://apidemo-7jd3lqpmk-mirezizs-projects.vercel.app/api/news`;
             async function mynews() {
                 let responce = await fetch(url);
                 let data = await responce.json();
                 console.log(data);
             }
-            mynews(); 
+            mynews();
         }
     })
-})
+});
